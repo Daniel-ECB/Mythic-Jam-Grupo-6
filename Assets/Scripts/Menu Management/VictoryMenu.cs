35cb2f335fb76424d831851d2a71649c70e429e7
Daniel Corzo <danielcorzo@outlook.com>
2025-05-30 00:30:38 -0300

    Added VictoryMenu functionality.

--- Assets/Scripts/Menu Management/VictoryMenu.cs
+++ Assets/Scripts/Menu Management/VictoryMenu.cs
@@ -1,7 +1,19 @@
-﻿namespace MythicGameJam.UI.Menus
+﻿using MythicGameJam.Core.GameManagement;
+using UnityEngine.SceneManagement;
+
+namespace MythicGameJam.UI.Menus
 {
     public sealed class VictoryMenu : UIMenu
     {
+        public void OnRestartButton()
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+            GameManager.Instance.LoadScene(currentScene, GameManager.Instance.StartGameplay);
+        }
 
+        public void OnMainMenuButton()
+        {
+            GameManager.Instance.LoadScene("MainMenu", GameManager.Instance.GoToHomeScreen);
+        }
     }
 }
